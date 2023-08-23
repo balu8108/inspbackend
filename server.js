7440c536303d5fc242e4a58f5852736eec98af60
Rhythm Yadav <y.rhythm786@gmail.com>
2023-08-23 17:38:45 +0530

    Backend updates

--- server.js
+++ server.js
@@ -1,50 +1,9 @@
-// const app = require("./app");
-
-// const mysql2 = require("mysql2");
-
-// const dotenv = require("dotenv");
-
-// dotenv.config({ path: "config/variable.env" });
-
-// const {sequelize}= require("./config/databaseConnection");
-
-// const PORT = process.env.PORT || 4000;
-
-// // const connection = mysql2.createConnection({
-// //   host: "localhost",
-// //   user: "Rhythm",
-// //   password: "Rhythm@123",
-// //   database: "INSP",
-// // });
-// // connection.connect((err) => {
-// //   if (err) {
-// //     console.error("Error connecting to the database: " + err.stack);
-// //     return;
-// //   }
-
-// //   console.log("Connected to the database sucessfully !!");
-// // });
-
-// sequelize
-//   .sync()
-//   .then(() => {
-//     console.log('Database synchronized.');
-//   })
-//   .catch((error) => {
-//     console.error('Error synchronizing the database:', error);
-//   });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running at port ${process.env.PORT}.`);
-// });
-
-
 const app = require("./app");
 const dotenv = require("dotenv");
 
 const { sequelize} = require("./config/databaseConnection");
 
-dotenv.config({ path: "config/variable.env" });
+dotenv.config({ path: "config/.env" });
 
 const PORT = process.env.PORT || 4000;
 
--- services/smsNotification.js
+++ services/smsNotification.js
@@ -1,12 +1,50 @@
+// const twilio = require('twilio');
+// const dotenv=require("dotenv");
+
+// dotenv.config({ path: "config/variable.env" });
+
+// const twilioPhoneNumber = '+16184924131';
+// const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+
+// const client = twilio(TWILIO_ACCOUNT_SID, "306a0ee3f0bbbee3c785982ae748f0d9");
+
+// async function sendMeetingInvitation(mobileNumber) {
+//   const message = `Hello! This is a reminder for our upcoming meeting. Please join us at the scheduled time. Looking forward to your participation!`;
+  
+//   try {
+//     const sentMessage = await client.messages.create({
+//       body: message,
+//       from: twilioPhoneNumber,
+//       to: mobileNumber
+//     });
+
+//     console.log(`Invitation message sent to ${mobileNumber}. Message SID: ${sentMessage.sid}`);
+//   } catch (error) {
+//     console.log('Error sending invitation message:', error);
+//     throw error;
+//   }
+// }
+// module.exports = {
+//   sendMeetingInvitation,
+// };
+
+
+
+
 const twilio = require('twilio');
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "config/.env" });
 
 const twilioPhoneNumber = '+16184924131';
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN; // Corrected this line
 
-const client = twilio("AC6496c0d388d3e43505d872d23848d158", "306a0ee3f0bbbee3c785982ae748f0d9");
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN); // Use TWILIO_AUTH_TOKEN here
 
 async function sendMeetingInvitation(mobileNumber) {
   const message = `Hello! This is a reminder for our upcoming meeting. Please join us at the scheduled time. Looking forward to your participation!`;
-  
+
   try {
     const sentMessage = await client.messages.create({
       body: message,
@@ -20,6 +58,17 @@ async function sendMeetingInvitation(mobileNumber) {
     throw error;
   }
 }
+
 module.exports = {
   sendMeetingInvitation,
 };
+
+
+
+
+
+
+
+
+
+
