de20300ea1288a31fbd7938719986fbd2402b37a
thapsamit <thapliyalamit2001@gmail.com>
2024-02-23 14:22:27 +0530

    added public ip

--- socketcontrollers/config.js
+++ socketcontrollers/config.js
@@ -3,14 +3,31 @@ const {
   MEDIA_SOUP_LISTEN_IP,
   MEDIA_SOUP_ANNOUNCED_IP,
 } = require("../envvar");
+
+const request = require("sync-request");
 const os = require("os");
-// announced ip is your local ip address as we are testing with react native so we need ip address
+function getMyIPSync() {
+  try {
+    const res = request("GET", "https://api64.ipify.org?format=json");
+    const body = JSON.parse(res.getBody("utf8"));
+    return body.ip;
+  } catch (error) {
+    console.error("Failed to get IP address:", error);
+    return null;
+  }
+}
+
+// Example usage
+const myIP = getMyIPSync();
+if (myIP) {
+  console.log(`Your public IP address is: ${myIP}`);
+}
+
 module.exports = Object.freeze({
   plainRtpTransport: {
     listenIp: {
       ip: MEDIA_SOUP_LISTEN_IP,
-      announcedIp:
-        ENVIRON !== "local" ? MEDIA_SOUP_ANNOUNCED_IP : "192.168.1.8",
+      announcedIp: ENVIRON !== "local" ? myIP : "192.168.1.8",
     }, // TODO: Change announcedIp to your external IP or domain name
     rtcpMux: false,
     comedia: false,
@@ -19,8 +36,7 @@ module.exports = Object.freeze({
     listenIps: [
       {
         ip: MEDIA_SOUP_LISTEN_IP,
-        announcedIp:
-          ENVIRON !== "local" ? MEDIA_SOUP_ANNOUNCED_IP : "192.168.1.8",
+        announcedIp: ENVIRON !== "local" ? myIP : "192.168.1.8",
       },
     ],
     enableUdp: true,
