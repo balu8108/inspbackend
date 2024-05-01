9055a4dbf671df79b9484b8ac8c3099e9a5ce835
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-05-01 15:03:11 +0530

    add cors

--- app.js
+++ app.js
@@ -74,7 +74,13 @@ app.use((req, res, next) => {
 });
 
 app.use(upload()); // this is required for uploading multipart/formData
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 
 app.use(routesConstants.SCHEDULE_LIVE_CLASS, scheduleLiveClass);
