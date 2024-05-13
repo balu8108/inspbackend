f5bbafe29b333facf968c7f6757208faf06e27d8
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-05-14 01:33:28 +0530

    remove cors

--- app.js
+++ app.js
@@ -73,11 +73,7 @@ app.use((req, res, next) => {
 });
 
 app.use(upload()); // this is required for uploading multipart/formData
-app.use(
-  cors({
-    origin: ALLOWED_ORIGINS,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 
